415ecfad1c764b712c0d7409d31a9942420f6ec4
jaswant7 <jaswantjassie7@gmail.com>
2023-04-14 15:47:31 +0530

    Make and Pre-Commit file

---
+++ Makefile
@@ -0,0 +1,6 @@
+# SHELL = /bin/bash
+
+.PHONY: help
+help:
+	@echo "Commands:"
+	@echo "venv    : creates a virtual environment."
--- src/utils.py
+++ src/utils.py
@@ -31,13 +31,13 @@ def evaluate_models(x_train,y_train,x_test,y_test,models,param):
 
             gs = GridSearchCV(model,para,cv=3)
             gs.fit(x_train,y_train)
-            
+
             model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
-            
+
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
-            
+
             train_model_score = r2_score(y_train,y_train_pred)
             test_model_score = r2_score(y_test,y_test_pred)
 
@@ -46,3 +46,9 @@ def evaluate_models(x_train,y_train,x_test,y_test,models,param):
     except Exception as e:
         raise CustomException(e,sys)
 
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
